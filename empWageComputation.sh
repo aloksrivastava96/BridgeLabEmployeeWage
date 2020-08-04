f18194404029c4ae1c7ef0e5be0b27967c95bc96
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-04 19:42:39 +0530

    added attendanceCheck method

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
+function attendanceCheck() {
+	echo $((RANDOM%2))
+}
+
+function empMain() {
+	attendance=$( attendanceCheck )
+	if ((attendance))
+	then
+		echo "Present"
+	else
+		echo "Absent"
+	fi
+}
 echo "Welcome to Employee Wage Computation Program"
+empMain
