94643778062be8a1b5be9ca1ad65f93eff9b3078
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 12:07:47 +0530

    Calculating wage of a employee for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -26,7 +26,7 @@ function empMain() {
 	EMP_WAGE_PER_HR=20
 	EMP_FULL_DAY_HR=8
 	EMP_PART_DAY_HR=4
-	NO_OF_WORK_DAY=7
+	NO_OF_WORK_DAY=20
 
 	totalEmpHr=0
 	totalWorkDay=0
@@ -39,7 +39,7 @@ function empMain() {
 		((totalWorkDay++))
         done
 	totalWage=$(($totalEmpHr*$EMP_WAGE_PER_HR))
-	echo "Total wage of the employee is:"$totalWage
+	echo "Total wage of a employee for this month is:"$totalWage
 }
 echo "Welcome to Employee Wage Computation Program"
 empMain
