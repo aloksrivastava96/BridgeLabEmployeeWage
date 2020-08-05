4980eb9d62d47f2c56a68808077cbbaa106249a4
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 11:45:03 +0530

    Part Time and Total Wage is added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,20 +5,34 @@ function attendanceCheck() {
 }
 
 function getDailyEmpWage() {
-	totalWorkDay=0
-	while [[ $totalWorkDay -lt $NO_OF_WORK_DAY ]]
-	do
-		attendance=$( attendanceCheck )
-		dailyWage[(( totalWorkDay++ ))]=$(( $attendance*$EMP_FULL_DAY_HR*$EMP_WAGE_PER_HR ))
-	done
-	echo ${dailyWage[@]}
+	dailyWage[$totalWorkDay]=$(( $attendance*$EMP_DAY_HR*$EMP_WAGE_PER_HR ))
+	echo ${dailyWage[totalWorkDay]}
 }
 
 function empMain() {
 	EMP_WAGE_PER_HR=20
 	EMP_FULL_DAY_HR=8
+	EMP_PART_DAY_HR=4
 	NO_OF_WORK_DAY=7
-	getDailyEmpWage $EMP_WAGE_PER_HR $EMP_FULL_DAY_HR $NO_OF_WORK_DAY
+
+	totalEmpHr=0
+	totalWorkDay=0
+	while [[ $totalWorkDay -lt $NO_OF_WORK_DAY ]]
+        do
+                isFullTime=$((RANDOM%2))
+                if ((isFullTime==1))
+                then
+                        EMP_DAY_HR=$EMP_FULL_DAY_HR
+                else
+                        EMP_DAY_HR=$EMP_PART_DAY_HR
+                fi
+                attendance=$( attendanceCheck )
+		totalEmpHr=$(( $totalEmpHr + (($attendance*$EMP_DAY_HR)) ))
+                echo "Emp Wage for this day is:"$( getDailyEmpWage $totalWorkDay $attendance $EMP_DAY_HR $EMP_WAGE_PER_HR )
+		((totalWorkDay++))
+        done
+	totalWage=$(($totalEmpHr*$EMP_WAGE_PER_HR))
+	echo "Total wage of the employee is:"$totalWage
 }
 echo "Welcome to Employee Wage Computation Program"
 empMain
