676825d5cff201a8fba744369469b26c8ffc14c9
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 10:53:37 +0530

    added getDailyEmpWage method

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,14 +4,21 @@ function attendanceCheck() {
 	echo $((RANDOM%2))
 }
 
+function getDailyEmpWage() {
+	totalWorkDay=0
+	while [[ $totalWorkDay -lt $NO_OF_WORK_DAY ]]
+	do
+		attendance=$( attendanceCheck )
+		dailyWage[(( totalWorkDay++ ))]=$(( $attendance*$EMP_FULL_DAY_HR*$EMP_WAGE_PER_HR ))
+	done
+	echo ${dailyWage[@]}
+}
+
 function empMain() {
-	attendance=$( attendanceCheck )
-	if ((attendance))
-	then
-		echo "Present"
-	else
-		echo "Absent"
-	fi
+	EMP_WAGE_PER_HR=20
+	EMP_FULL_DAY_HR=8
+	NO_OF_WORK_DAY=7
+	getDailyEmpWage $EMP_WAGE_PER_HR $EMP_FULL_DAY_HR $NO_OF_WORK_DAY
 }
 echo "Welcome to Employee Wage Computation Program"
 empMain
