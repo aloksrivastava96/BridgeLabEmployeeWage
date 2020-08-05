11e34b7fada78e8fba47154a8cc04d3261c910f1
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-05 12:21:38 +0530

    Maximum limit of work hour and work day for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -27,10 +27,11 @@ function empMain() {
 	EMP_FULL_DAY_HR=8
 	EMP_PART_DAY_HR=4
 	NO_OF_WORK_DAY=20
+	MAX_MONTH_HR=100
 
 	totalEmpHr=0
 	totalWorkDay=0
-	while [[ $totalWorkDay -lt $NO_OF_WORK_DAY ]]
+	while [[ $totalEmpHr -lt $MAX_MONTH_HR && $totalWorkDay -lt $NO_OF_WORK_DAY ]]
         do
                 isFullTime=$((RANDOM%2))
 		EMP_DAY_HR=$( getWorkingHr $isFullTime )
@@ -39,6 +40,7 @@ function empMain() {
 		((totalWorkDay++))
         done
 	totalWage=$(($totalEmpHr*$EMP_WAGE_PER_HR))
+	echo "Total Working Hour for this month is:"$totalEmpHr
 	echo "Total wage of a employee for this month is:"$totalWage
 }
 echo "Welcome to Employee Wage Computation Program"
